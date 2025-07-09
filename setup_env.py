5ec9ac45a18c549283265974aaf8902e2e18a7d8
Steffen Peikert <steffen.peikert@fau.de>
2025-07-09 17:07:16 +0200

    Fix data download
    Need to test with the actual URL - but wiht a test repo it works fine

--- setup_env.py
+++ setup_env.py
@@ -1,6 +1,7 @@
 import subprocess
 from pathlib import Path
 import argparse
+import sys
 
 
 def download_url(url: str, save_path: Path, chunk_size=128):
@@ -14,7 +15,13 @@ def download_url(url: str, save_path: Path, chunk_size=128):
     r = requests.get(url, stream=True)
     size = r.headers["content-length"]
     length = int(int(size) / chunk_size)
-    with open(str(save_path.absolute()), "wb") as fd, progress(
+
+    import tqdm
+    import os
+
+    os.makedirs(save_path.parent, exist_ok=True)
+
+    with open(str(save_path.absolute()), "wb") as fd, tqdm.tqdm(
         total=length * chunk_size,
         unit_scale=True,
         unit_divisor=1024,
@@ -24,6 +31,26 @@ def download_url(url: str, save_path: Path, chunk_size=128):
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
             pbar.update(chunk_size)
+
+
+def get_data():
+
+    download_url(
+        "https://getsamplefiles.com/download/zip/sample-1.zip",
+        Path("tmp/data.zip"),
+    )
+
+    # Unzip the downloaded file
+    import zipfile
+
+    with zipfile.ZipFile("tmp/data.zip", "r") as zip_ref:
+        zip_ref.extractall("data")
+
+    # Clean up the temp folder
+    import shutil
+
+    if Path("tmp").exists():
+        shutil.rmtree("tmp")
 
 
 def use_conda():
@@ -44,6 +71,10 @@ def use_conda():
     except:
         print("[SPARC WARNING]: Conda environment already exists.")
 
+    subprocess.check_call(
+        ["conda", "run", "-n", "workshop", "python", "setup_env.py", "--download"]
+    )
+
     print("[SPARC INFO]: Done!")
 
 
@@ -60,11 +91,13 @@ def use_venv():
     )
 
     venv.create(".venv", with_pip=True)
+    venv_python_path = Path(".venv") / "bin" / "python"
 
     venv_pip_path = Path(".venv") / "bin" / "pip"
     if not venv_pip_path.exists():
         # Try a windows compatible path
         venv_pip_path = Path(".venv") / "Scripts" / "pip.exe"
+        venv_python_path = Path(".venv") / "Scripts" / "python.exe"
         if not venv_pip_path.exists():
             raise RuntimeError("[SPARC ERROR]: Could not find the virtual environment.")
 
@@ -100,6 +133,15 @@ def use_venv():
         "[SPARC INFO]: If you would like to use the GPU version of pytorch, please install the correct one manually from here: https://pytorch.org/get-started/locally/"
     )
 
+    # Use the venv and call the download_url function to download the datasets
+    subprocess.check_call(
+        [
+            str(venv_python_path),
+            "setup_env.py",
+            "--download",
+        ]
+    )
+
     print("[SPARC INFO]: Done!")
 
 
@@ -111,9 +153,18 @@ if __name__ == "__main__":
         action="store_true",
         help="Use conda to create the environment.",
     )
+    parser.add_argument(
+        "--download",
+        action="store_true",
+        help="Get the workshop data.",
+    )
+
     args = parser.parse_args()
 
-    if args.conda:
+    if args.download:
+        get_data()
+        sys.exit(0)
+    elif args.conda:
         use_conda()
     else:
         use_venv()
