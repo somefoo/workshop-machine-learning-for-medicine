e0d0f8730f44556d17cb3e51ed888d7db0b9065c
Steffen Peikert <steffen.peikert@fau.de>
2025-07-10 18:01:36 +0200

    Fix/optional venv (#1)
    * Update setup script to enable both venv and conda
    Also update ReadMe to reflect changes
    
    * Fix data download
    
    * Fix code errors
    Some code-parts were not executing correctly (e.g., wrong variable
    names), or they were using deprecated parameters.

--- README.md
+++ README.md
@@ -3,20 +3,21 @@
 * Ubuntu 18.04 or MacOSX. Windows 10 might work as well, but was not tested.
 * git ([installation instructions](https://git-scm.com/downloads))
 * Python 3.7 or later ([download](https://www.python.org/downloads/))
-* [Miniconda](https://docs.conda.io/en/latest/miniconda.html) or [Anaconda](https://docs.anaconda.com/anaconda/install/)
+* *optional* [Miniconda](https://docs.conda.io/en/latest/miniconda.html) or [Anaconda](https://docs.anaconda.com/anaconda/install/)
 
 ## Installation
 1. Clone this repository. `git@github.com:somefoo/workshop-machine-learning-for-medicine.git`
 2. Go into the cloned folder. `cd workshop-machine-learning-for-medicine`
-3. Execute the setup script `python3 setup_env.py`. This will create a new conda environment, install all required packages and download a dataset. If you do not want to use the gpu version of pytorch, call the script as `python3 setup_env.py --cpu`. This will be the case if you are on a laptop or desktop without a strong dedicated graphics card. Sadly, the original server hosting the dataset doesn't exist anymore. Ask me for the original data (we will try to host it ourselves).
-4. Activate the conda environment `conda activate workshop`
-5. Start a jupyter notebook server `jupyter notebook`.
-6. Open chrome or firefox and go to `http://localhost:8888/tree`.
-7. Open the `Introduction Tutorial.ipynb` notebook to get started.
-8. Continue to the `Skin Cancer Classification.ipynb` notebook.
+3. Make sure you are within the FAU intranet, or the FAU VPN, otherwise the data download during the following setup will not work.
+4. Execute the setup script `python3 setup_env.py`. Per default, this will create a virtual environment with all the required packages and download a dataset. If you would rather use a conda/miniconda environment, use `python3 setup_env.py --conda`. By default, both the venv and conda option will install the pytorch for CPU. This will be the case if you are on a laptop or desktop without a strong dedicated graphics card. If you would like to use the GPU version instead, please manually install the correct version from [here](https://pytorch.org/get-started/locally/) **AFTER** executing the setup script and sourcing your environment.
+5. Activate the environment; either `source .venv/bin/activate` or the conda environment `conda activate workshop`
+6. Start a jupyter notebook server `jupyter notebook`.
+7. Open chrome or firefox and go to `http://localhost:8888/tree`.
+8. Open the `Introduction Tutorial.ipynb` notebook to get started.
+9. Continue to the `Skin Cancer Classification.ipynb` notebook.
 
 ## Information
-This workshop is part of the lecture Surgical Technology Innovation. More information on [SPARC](https://www.sparc.tf.fau.de/) on our websites.
+This workshop is part of the lecture Surgical Technology Innovation and Robotics in Surgery and Diagnostics. More information on [SPARC](https://www.sparc.tf.fau.de/) on our websites.
 
 ## Source
 This workshop is based on the workshop by Paul Maria Scheikl presented at CURAC 2021: [Link](https://github.com/ScheiklP/CURAC-Academy-2021)
--- setup_env.py
+++ setup_env.py
@@ -1,40 +1,184 @@
 import subprocess
 from pathlib import Path
-import zipfile
-
-try:
-    import requests
-except ModuleNotFoundError:
-    raise ModuleNotFoundError("[SPARC ERROR]: please install the requests package before continuing.\n \t pip3 install requests")
+import argparse
+import sys
 
 
 def download_url(url: str, save_path: Path, chunk_size=128):
+    try:
+        import requests
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "[SPARC ERROR]: please install the requests package before continuing.\n \t pip3 install requests"
+        )
+
     r = requests.get(url, stream=True)
-    size = r.headers["content-length"]
-    length = int(int(size) / chunk_size)
-    with open(str(save_path.absolute()), "wb") as fd, progress(
-        total=length * chunk_size, unit_scale=True, unit_divisor=1024, unit="B", desc=save_path.name
+    size = r.headers.get("content-length")
+    if size is None:
+        total = None
+    else:
+        total = int(size)
+
+    import tqdm
+    import os
+
+    os.makedirs(save_path.parent, exist_ok=True)
+
+    with open(str(save_path.absolute()), "wb") as fd, tqdm.tqdm(
+        total=total,
+        unit_scale=True,
+        unit_divisor=1024,
+        unit="B",
+        desc=save_path.name,
     ) as pbar:
         for chunk in r.iter_content(chunk_size=chunk_size):
             fd.write(chunk)
             pbar.update(chunk_size)
 
 
-if __name__ == "__main__":
+def get_data():
+
+    # Wait for the user to confirm that we are within a FAU network
+    print(
+        "[SPARC INFO]: Please make sure you are connected to the FAU network (e.g., VPN) before downloading the data."
+    )
+    confirm = input("Continue? (Y/n)").strip().lower()
+    if confirm not in ["y", "yes", ""]:
+        print(
+            "[SPARC INFO]: Exiting without downloading data. Please run again with --download to download the data."
+        )
+        sys.exit(0)
+
+    download_url(
+        "https://sparc-cloud.aibe.uni-erlangen.de/f/d5579673d3b24f36adfe/?dl=1",
+        Path("tmp/data.zip"),
+    )
+
+    # Unzip the downloaded file
+    import zipfile
+
+    with zipfile.ZipFile("tmp/data.zip", "r") as zip_ref:
+        zip_ref.extractall("data")
+
+    # Clean up the temp folder
+    import shutil
+
+    if Path("tmp").exists():
+        shutil.rmtree("tmp")
+
+
+def use_conda():
     try:
         out = subprocess.check_output(["conda", "--version"])
     except OSError:
-        raise RuntimeError("Conda (Miniconda or Anaconda) must be installed. https://docs.conda.io/en/latest/miniconda.html")
+        raise RuntimeError(
+            "Conda (Miniconda or Anaconda) must be installed. https://docs.conda.io/en/latest/miniconda.html"
+        )
 
-    print("[SPARC INFO]: Creating a new conda environment named workshop and installing packages...")
+    print(
+        "[SPARC INFO]: Creating a new conda environment named workshop and installing packages..."
+    )
     print(f"[SPARC INFO]: Pytorch will be installed (cpu only support).")
 
     try:
-        subprocess.check_call(
-            ["conda", "env", "create", "--file", "conda_cpu.yml"]
-        )
+        subprocess.check_call(["conda", "env", "create", "--file", "conda_cpu.yml"])
     except:
         print("[SPARC WARNING]: Conda environment already exists.")
-        
+
+    subprocess.check_call(
+        ["conda", "run", "-n", "workshop", "python", "setup_env.py", "--download"]
+    )
 
     print("[SPARC INFO]: Done!")
+
+
+def use_venv():
+    try:
+        import venv
+    except ImportError:
+        raise ImportError(
+            "Python venv module is not available. Please install Python 3.3 or later."
+        )
+
+    print(
+        "[SPARC INFO]: Creating a new virtual environment named .venv and installing packages..."
+    )
+
+    venv.create(".venv", with_pip=True)
+    venv_python_path = Path(".venv") / "bin" / "python"
+
+    venv_pip_path = Path(".venv") / "bin" / "pip"
+    if not venv_pip_path.exists():
+        # Try a windows compatible path
+        venv_pip_path = Path(".venv") / "Scripts" / "pip.exe"
+        venv_python_path = Path(".venv") / "Scripts" / "python.exe"
+        if not venv_pip_path.exists():
+            raise RuntimeError("[SPARC ERROR]: Could not find the virtual environment.")
+
+    subprocess.check_call(
+        [
+            str(venv_pip_path),
+            "install",
+            "livelossplot",
+            "torchsummary",
+            "numpy",
+            "notebook",
+            "ipywidgets",
+            "pandas",
+            "pillow",
+            "tqdm",
+            "matplotlib",
+            "requests",
+        ]
+    )
+
+    print("[SPARC INFO]: Downloading and installing PyTorch (cpu only support)...")
+    subprocess.check_call(
+        [
+            str(venv_pip_path),
+            "install",
+            "torch",
+            "torchvision",
+            "torchaudio",
+        ]
+    )
+
+    print(
+        "[SPARC INFO]: If you would like to use the GPU version of pytorch, please install the correct one manually from here: https://pytorch.org/get-started/locally/"
+    )
+
+    # Use the venv and call the download_url function to download the datasets
+    subprocess.check_call(
+        [
+            str(venv_python_path),
+            "setup_env.py",
+            "--download",
+        ]
+    )
+
+    print("[SPARC INFO]: Done!")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Setup environment for the workshop.")
+    parser.add_argument(
+        "--conda",
+        action="store_true",
+        help="Use conda to create the environment.",
+    )
+    parser.add_argument(
+        "--download",
+        action="store_true",
+        help="Get the workshop data.",
+    )
+
+    args = parser.parse_args()
+
+    if args.download:
+        get_data()
+        sys.exit(0)
+    elif args.conda:
+        use_conda()
+    else:
+        use_venv()
